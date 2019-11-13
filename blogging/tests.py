5dde6388776f3e3dd350007bcd7409c65f50c264
lolaguer <lolaguerrero2015@gmail.com>
2019-11-13 15:13:14 -0800

    adding categories

--- blogging/tests.py
+++ blogging/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# another import
+from blogging.models import Category
+
+# and the test case and test
+class CategoryTestCase(TestCase):
+
+    def test_string_representation(self):
+        expected = "A Category"
+        c1 = Category(name=expected)
+        actual = str(c1)
+        self.assertEqual(expected, actual)
