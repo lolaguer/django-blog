0b76f67fed3ce6927cafac8b5b7e898d5b2c2b09
lolaguer <lolaguerrero2015@gmail.com>
2019-11-13 15:47:29 -0800

    new view

--- blogging/views.py
+++ blogging/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.template import loader
+from blogging.models import Post
 
 # Create your views here.
 
@@ -13,3 +15,21 @@ def stub_view(request, *args, **kwargs):
         body += "Kwargs:\n"
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
+
+## and this view
+#def list_view(request):
+#    published = Post.objects.exclude(published_date__exact=None)
+#    posts = published.order_by('-published_date')
+#    template = loader.get_template('blogging/list.html')
+#    context = {'posts': posts}
+#    body = template.render(context)
+#    return HttpResponse(body, content_type="text/html")
+
+
+# rewrite our view
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    context = {'posts': posts}
+    return render(request, 'blogging/list.html', context)
+
