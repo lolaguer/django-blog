ab3cd24ea3b91fefa7ab4a0fedbf8f09e0490554
lolaguer <lolaguerrero2015@gmail.com>
2019-11-28 08:59:25 -0800

    Fixing requirements.txt

---
+++ .djangoenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/lolaguerrero/Documents/python 3 course/django-blog/.djangoenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
