d349ac46a619c3ab9f7fb1f5832ed4c87ee7b93a
lolaguer <lolaguerrero2015@gmail.com>
2019-12-01 18:31:24 -0800

    assig L07

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
-
-# a new import
 from blogging.models import Post, Category
 
-# and a new admin registration
-admin.site.register(Post)
-admin.site.register(Category)
+## and a new admin registration
+# admin.site.register(Post)
+# admin.site.register(Category)
+
+
+class CategoryInLine(admin.TabularInline):
+    model = Category.posts.through
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+       CategoryInLine,
+    ]
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [
+       CategoryInLine, 
+    ]    
+    exclude = ('posts',) 
